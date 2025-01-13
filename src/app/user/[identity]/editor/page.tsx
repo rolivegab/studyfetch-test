f0ba37c0372e10955b00923014e07fb3c05932e7
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2025-01-13 15:08:03 -0300

    add copy link to share and change room

--- src/app/user/[identity]/editor/page.tsx
+++ src/app/user/[identity]/editor/page.tsx
@@ -2,15 +2,38 @@
 
 import Editor from "@/components/editor/editor.component";
 import { Box, Typography } from "@mui/material";
-import { style } from "./page.style";
+import Link from "next/link";
 import { useParams } from "next/navigation";
+import { useRef } from "react";
+import { style } from "./page.style";
 
 const EditorPage = () => {
   const params = useParams<{ identity: string }>();
+  const linkRef = useRef<HTMLAnchorElement>(null);
 
   return (
     <Box sx={style}>
       <Typography variant="h5">Room ID {params?.identity}</Typography>
+      <div>
+        <Link href="/" style={{ textDecoration: "underline" }}>
+          Click to change room
+        </Link>
+        <br />
+        <Link
+          href="/"
+          ref={linkRef}
+          style={{ textDecoration: "underline" }}
+          onClick={(e) => {
+            e.preventDefault();
+            navigator.clipboard.writeText(window.location.href);
+            if (linkRef.current) {
+              linkRef.current.textContent = "Copied!";
+            }
+          }}
+        >
+          Click to copy share link
+        </Link>
+      </div>
       <Box mt={2} />
       {params?.identity && <Editor documentName={params.identity} />}
     </Box>
